d086995eb95e15d93c24fc2e719141057b6fa2dc
Ariel <aalvarez@okkralabs.com>
2016-09-04 13:01:53 -0400

    unestable, refactoring structured programming to OOP

--- scripts/data_formatter.py
+++ scripts/data_formatter.py
@@ -1,89 +1,78 @@
-import ConfigParser
 import copy
-import json
-
-configParser = ConfigParser.ConfigParser()
-configParser.read("./config.txt")
-break_after = 1000
-break_after_limit = False
-
-k_neighbors = configParser.get("config", 'k_neighbors')
 
 
-rating_file_path = "{}/{}".format(configParser.get("config", "data_folder"), configParser.get("config", "original_rating_file") )
-output_movies_folder = "{}/{}".format(configParser.get("config", "data_folder"), configParser.get("config", "output_movies_folder"))
-output_users_folder = "{}/{}".format(configParser.get("config", "data_folder"), configParser.get("config", "output_users_folder"))
-
-rating_file_order = ["user_id","movie_id","rating","timestamp"]
-
-movieFileTpl = {
-                
-    "mean_rating" : "3.3",
-
-    "ratings" : {
-             
-                "user_id" : "3.3"
-    },
-                
-    "k_neighbors": {
-            "movie_id" : "3.3 (weight)"
-    }
-                
-}
-
-
-
-userFileTpl = {
-    "ratings": {
-        "movie-id" : 2.2
-    }
-}
-
-all_movie_data = {
-              
-}
-
-all_user_data = {
-
-}
-
-
-def read_original_data(all_movie_data, all_user_data):    
-    with open(rating_file_path) as infile:
-        for i, line in enumerate(infile):
-            vals = line.split("::")
-            
-            vals = dict( zip(rating_file_order, vals) )
-            user_id = vals['user_id']
-            movie_id = vals['movie_id']
-                    
-            movie_data =   all_movie_data[movie_id] if movie_id in all_movie_data else copy.deepcopy(movieFileTpl) 
-            user_data = all_user_data[user_id] if user_id in all_user_data else copy.deepcopy(userFileTpl) 
-            
-                   
-            movie_data['ratings'][user_id] = vals['rating']
-            
-            user_data['ratings'] [movie_id] = vals['rating']
-            
-            all_user_data[user_id] = user_data
-            all_movie_data[movie_id] = movie_data       
-            if(i >= break_after and break_after_limit):
-                break;
-
-
-def write_to_file (data_dict, output_folder):
-    print(data_dict)
-    for key, value in data_dict.iteritems():
-        file = open("{}/{}.{}".format(output_folder, key, 'json'),'w+')
-        json.dump(value, file)
-        file.close()
+class DataFormatter(object):
+    
+    def __init__(self, break_after = 1000, break_after_limit=False):
+        self.break_after = self.break_after
+        self.break_after_limit = self.break_after_limit
         
-            
-read_original_data(all_movie_data, all_user_data)
-write_to_file(all_user_data, output_users_folder)
-write_to_file(all_movie_data, output_movies_folder)
-
+        
+       
+        
+        self.rating_file_order = ["user_id","movie_id","rating","timestamp"]
+        
+        self.movie_file_tpl = {
+                        
+            "mean_rating" : "3.3",
+        
+            "ratings" : {
+                     
+                        "user_id" : "3.3"
+            },
+                        
+            "k_neighbors": {
+                    "movie_id" : "3.3 (weight)"
+            }
+                        
+        }
+        
+        
+        
+        self.movie_file_tpl = {
+            "ratings": {
+                "movie-id" : 2.2
+            }
+        }
+        
+        self.all_movie_data = {
+                      
+        }
+        
+        self.all_user_data = {
+        
+        }
+    
+        
+    
+    def read_original_data(self):    
+        with open(self.rating_file_path) as infile:
+            for i, line in enumerate(infile):
+                vals = line.split("::")
                 
-print all_movie_data
-print all_user_data
-            
+                vals = dict( zip(self.rating_file_order, vals) )
+                user_id = vals['user_id']
+                movie_id = vals['movie_id']
+                        
+                movie_data =   self.all_movie_data[movie_id] if movie_id in self.all_movie_data else copy.deepcopy(self.movie_file_tpl) 
+                user_data = self.all_user_data[user_id] if user_id in self.all_user_data else copy.deepcopy(self.movie_file_tpl) 
+                
+                       
+                movie_data['ratings'][user_id] = vals['rating']
+                
+                user_data['ratings'] [movie_id] = vals['rating']
+                
+                self.all_user_data[user_id] = user_data
+                self.all_movie_data[movie_id] = movie_data       
+                if(i >= self.break_after and self.break_after_limit):
+                    break;
+    
+   
+    def ori_data_to_json_files(self):
+        self.read_original_data()
+        self.write_to_json_files(self.all_user_data, self.output_users_folder)
+        self.write_to_json_files(self.all_movie_data, self.output_movies_folder)      
+                
+    
+    
+                
--- scripts/main.py
+++ scripts/main.py
@@ -1,128 +1,13 @@
+from data_formatter import DataFormatter
+from  k_neighbors import KNeighbors
+from data_analyst import DataAnalyst
 
 
-""" Folder data\raw\recipes_reviews has files with the recipe_id as the name of the file.
-Inside each files are the reviews for that particular recipe. That is the data I got from the BigOven.com. """
+analyst = DataAnalyst()
+analyst.provide_analysis()
 
+#data_formatter  = DataFormatter()
+#data_formatter.ori_data_to_json_files()
 
-import ConfigParser
-import pandas as pd
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as pl
-from datetime import datetime
-
-from scipy.stats import pearsonr
-from rope.refactor import similarfinder
-import random
-
-configParser = ConfigParser.ConfigParser()
-configParser.read("./config.txt")
-data_folder = configParser.get("config", "data_folder")
-ratings_file = configParser.get("config", "original_rating_file")
-ratings_file_path = "{}/{}".format(data_folder, ratings_file)
-
-all_data = pd.read_table(ratings_file_path, sep="::", skiprows=0, header=None, names=["user_id", "movie_id", "rating", "timestamp"])
-
-train_percentages = [0.3, 0.5, 0.7, 1]
-k_vals = [10, 20, 30, 40]
-test_columns = ['Train', 'K' , 'MAE', 'Neighbors Time', '' , 'Throughput']
-all_movies_ids = all_data['movie_id'].unique()
-
-results = pd.DataFrame(columns=test_columns)
-
-matrix_J_U = all_data.pivot(columns="user_id", index="movie_id", values="rating")
-movies_ids = matrix_J_U.index.values 
-
-matrix_J_U = matrix_J_U.values
-
-time_neighbors = 0
-user_count = matrix_J_U.shape[1] 
-product_count = matrix_J_U.shape[0]
-range_of_rows = range(0, product_count)
-neighbors_dict = {}
-
-def save_to_dict(id_i,id_j,pc):
-    
-    id_i = str(id_i)
-    id_j = str(id_j)
-    
-    neighbors_dict[id_i] = neighbors_dict[id_i] if id_i in neighbors_dict else {}
-    neighbors_dict[id_j] = neighbors_dict[id_j] if id_j in neighbors_dict else {}
-     
-    neighbors_dict[id_i][id_j] = pc
-    neighbors_dict[id_j][id_i] = pc 
-
-def find_nearest_neighbors(matric_J_U):
-    
-    init_time = datetime.now()
-    
-    for i in range(0, matrix_J_U.shape[0]):
-        for j in range(i+1, matrix_J_U.shape[0]):
-            pearson_ij = pearsonr(matrix_J_U[i], matrix_J_U[j])
-            movie_i_id = movies_ids[i]            
-            movie_j_id = movies_ids[j]
-            save_to_dict(movie_i_id,movie_j_id,pearson_ij)           
-            
-    timeDelta = init_time - datetime.now() 
-    return  (neighbors_dict, timeDelta.total_seconds())      
-
-
-
-    
-
-for train_percentage in train_percentages:
-    for k_val in k_vals:
-        subset_rows = int(matrix_J_U.shape[0] * train_percentage)
-                
-        movies_subset = matrix_J_U[ random.sample(range_of_rows,  subset_rows) , :]
-        neighbors_dict, time_neighbors  = find_nearest_neighbors(movies_subset)
-        
-        #write_to_files(neighbors_dict)
-        results.append({"Train" : train_percentage, "Neighbors Time" : time_neighbors} , ignore_index=True)
-        break
-    break
-
-
-print "Neighbors:",neighbors_dict
-print results
-
-
-        
-
-
-    
-
-
-
-
-
-
-def visualize_dist (df):
-    # Visualization of data
-    ratings_dist = df.groupby("rating")
-    X = ratings_dist.groups.keys()
-    Y = ratings_dist.count()['movie_id']
-    
-    font = {'family' : 'normal',
-            'weight' : 'bold',
-            'size'   : 34}
-    
-    matplotlib.rc('font', **font)
-    
-    sub_plot = pl.subplot()
-    sub_plot.bar(X, Y, align="center")
-    sub_plot.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ","))) 
-    
-    
-    pl.xlabel("Ratings", size=34, weight="bold")
-    pl.ylabel("Frequency", size=34, weight="bold")
-    
-    pl.show()
-    
-    print(df['user_id'])
-    print(df.describe())
-    
-        
-        
-    
-    
+#neighbors_calc = KNeighbors()
+#neighbors_calc.compute_k_neighbors()
