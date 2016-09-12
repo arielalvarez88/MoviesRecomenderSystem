a6f755eb06502f26b9d1e995cf744712735c63f0
Ariel <aalvarez@okkralabs.com>
2016-09-12 00:26:08 -0400

    adding plots of the results

--- scripts/data_analyst.py
+++ scripts/data_analyst.py
@@ -1,12 +1,14 @@
 import matplotlib
-
+import matplotlib.pyplot as plt
 from data_io import DataReader
-import matplotlib.pyplot as pl
 
 
 class DataAnalyst(object):
     """Provides analytics of the data.
     """
+    def __init__(self):
+        self.report_metrics = ['Train_percentage', 'K_neighbors' , 'MAE', 'PC_calc_time', 'Throughput', "Time_MAE"]
+
     
     def provide_analysis(self, data_frame = None):
         """Provides visualization of the data's dist. and the data's statistic info.
@@ -42,12 +44,43 @@ class DataAnalyst(object):
         
         matplotlib.rc('font', **font)
         
-        sub_plot = pl.subplot()
+        sub_plot = plt.subplot()
         sub_plot.bar(X, Y, align='center')
         sub_plot.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ','))) 
         
         
-        pl.xlabel('Ratings', size=34, weight='bold')
-        pl.ylabel('Frequency', size=34, weight='bold')
+        plt.xlabel('Ratings', size=34, weight='bold')
+        plt.ylabel('Frequency', size=34, weight='bold')
         
-        pl.show()
+        plt.show()
+        
+    def simple_graph(self, xlabel,ylabel, xdata, ydata):
+        fig = plt.figure()
+        fig.add_subplot(111, xlabel = xlabel, ylabel=ylabel)        
+        plt.plot(xdata,ydata,figure=fig)
+        
+        
+    def graph_results(self,results):
+        print "Brumm brumm"
+        """Generate graphs showing results
+        Args:
+            dict: The results containing the following values.
+        """        
+        
+        #K vs MAE         
+        self.simple_graph("Number of Neighbors","MAE", results['K_neighbors'], results['MAE'])
+                                
+        #Train-Percentage vs MAE 
+        self.simple_graph("Train_percentage","MAE", results['Train_percentage'], results['MAE'])
+        
+        #K vs Throughput 
+        self.simple_graph("Number of Neighbors","Throughput", results['K_neighbors'], results['Throughput'])
+        
+        #Train-Precentage vs PC_calc_time
+        self.simple_graph("Train_percentage","PC_calc_time", results['Train_percentage'], results['PC_calc_time'])
+        
+        #Train_percentage vs Time_MAE 
+        self.simple_graph("Train_percentage","Time_MAE", results['Train_percentage'], results['Time_MAE'])
+         
+
+        plt.show()
--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,7 @@ from metrics_calc import MetricsCalc
 
 dataframe = DataReader().read_ratings_as_df()
 analyst = DataAnalyst()
-analyst.provide_analysis()
+#analyst.provide_analysis()
 
 #data_formatter  = DataFormatter()
 #data_formatter.ori_data_to_json_files(dataframe)
@@ -14,6 +14,6 @@ analyst.provide_analysis()
 
 metrics_calc = MetricsCalc()
 
-results = metrics_calc.grid_search(dataframe,[0.1], [2])
+results = metrics_calc.grid_search(dataframe,[0.2,0.4,0.6,0.8], [5,10,20,30,40,50,60])
 
-analyst.show_results(results)
+analyst.graph_results(results)
